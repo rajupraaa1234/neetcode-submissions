122d0e3aaf30ac3bf72ebaf4052858484a4891a6
Raju kumar <48593134+rajupraaa1234@users.noreply.github.com>
2026-07-11 18:08:51 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findDuplicate(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+       for(int i=0;i<arr.length;i++){
+           if(set.contains(arr[i])) return arr[i];
+           set.add(arr[i]);
+       }
+        return -1;
+    }
+}
